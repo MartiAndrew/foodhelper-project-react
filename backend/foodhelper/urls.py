39127167959c843158cb1b4f4a0a7cc65b1dc0c4
Andrew <andrew.mishkov@gmail.com>
2023-06-29 00:33:55 +0300

    add  CustomUser Model

--- backend/foodhelper/urls.py
+++ backend/foodhelper/urls.py
@@ -1,21 +1,40 @@
-"""foodhelper URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path, re_path
+from django.views.generic import TemplateView
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+DEFAULT_VERSION = 'v1'
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Foodhelper',
+        default_version=DEFAULT_VERSION,
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        re_path(
+            r'^swagger/$',
+            schema_view.with_ui('swagger', cache_timeout=0),
+            name='schema-swagger-ui',
+        ),
+        path('redoc/',
+             TemplateView.as_view(template_name='redoc.html'),
+             name='redoc'),
+    ]
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,36 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    """Модель пользователя"""
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        verbose_name='Уникальный никнэйм',
+        error_messages={
+            'unique': "Пользователь с таким никнеймом уже существует.",
+        }
+    )
+    email = models.EmailField(
+        max_length=254, unique=True, verbose_name='Почта')
+    first_name = models.CharField(
+        max_length=150, verbose_name='Имя'
+    )
+    last_name = models.CharField(max_length=150,
+                                 verbose_name='Фамилия')
+
+    class Meta:
+        ordering = ('id',)
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
+
+
+
+
+
