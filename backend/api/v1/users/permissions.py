de9af288be3d5e71caf9446b8939062c74907970
Andrew <andrew.mishkov@gmail.com>
2023-07-11 00:29:09 +0300

    fix permissions

--- backend/api/v1/users/permissions.py
+++ backend/api/v1/users/permissions.py
@@ -1,13 +1,16 @@
 from rest_framework import permissions
 
 
-class IsAdminOrAuthorOrReadOnly(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return (request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated)
-
+class IsAuthorOrAdminOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS
-                or obj.author == request.user
-                or request.user.is_staff)
+        return (
+                obj.author == request.user
+                or request.method in permissions.SAFE_METHODS
+                or request.user.is_superuser)
 
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return (
+                request.method in permissions.SAFE_METHODS
+                or request.user.is_staff)
