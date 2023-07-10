a778cb9bc6baed453f0a70196a979a1ab2584854
Andrew <andrew.mishkov@gmail.com>
2023-07-11 00:27:47 +0300

    fix urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
+from .v1.favorites.views import FavoriteCreateDel
+from .v1.shopping_cart.views import ShoppingCartCreateDel
 from .v1.users.views import CustomUserViewSet
 from .v1.tags.views import TagViewSet
 from .v1.ingredient.views import IngredientViewSet
@@ -15,6 +17,12 @@ router_v1.register('ingredients', IngredientViewSet, basename='ingredients')
 router_v1.register('recipes', RecipeViewSet, basename='recipes')
 
 urlpatterns = [
+    path('recipes/<int:recipe_id>/favorite/',
+          FavoriteCreateDel.as_view(),
+          name='favorite_recipe'),
+    path('recipes/<int:recipe_id>/shopping_cart/',
+          ShoppingCartCreateDel.as_view(),
+          name='shopping_cart'),
     path('', include(router_v1.urls)),
     path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
