3d330392edb26ec57ec55ebbdaa5c0e4034248ed
Andrew <andrew.mishkov@gmail.com>
2023-07-11 00:28:53 +0300

    fix mixins

--- backend/api/v1/recipe/mixins.py
+++ backend/api/v1/recipe/mixins.py
@@ -1,24 +1,19 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import status
-from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+
+from api.v1.users.serializers import SubscriptionsRecipeSerializer
+from recipes.models import Recipe
 
 
-class FavoriteShoppingMixin:
-    """Класс-миксин общий для представлений списка избранных
-    рецептов и корзины покупок."""
+class GetObjectMixin:
+    """Миксина для удаления/добавления рецептов избранных/корзины."""
 
-    def perform_action(
-            self, user, recipe, serializer_class, model_class, request):
-        serializer = serializer_class(
-            recipe, data=request.data, context={'request': request})
-        if serializer.is_valid():
-            model_class.objects.create(user=user, recipe=recipe)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+    serializer_class = SubscriptionsRecipeSerializer
+    permission_classes = (AllowAny,)
 
-    def delete_action(self, user, recipe, model_class):
-        favorite_recipe = get_object_or_404(
-            model_class, user=user, recipe=recipe
-        )
-        favorite_recipe.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def get_object(self):
+        recipe_id = self.kwargs['recipe_id']
+        recipe = get_object_or_404(Recipe, id=recipe_id)
+        self.check_object_permissions(self.request, recipe)
+        return recipe
+
