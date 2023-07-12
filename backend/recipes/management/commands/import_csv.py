c6f3908672d89d35d1e49f5d21d554d00c2c0aa0
Andrew <andrew.mishkov@gmail.com>
2023-07-12 16:27:01 +0300

    fix import_csv

--- backend/recipes/management/commands/import_csv.py
+++ backend/recipes/management/commands/import_csv.py
@@ -1,34 +1,25 @@
 import csv
 
-from django.conf import settings
-from django.core.management.base import BaseCommand
+from django.core.management import BaseCommand
+from foodhelper.settings import BASE_DIR
+from recipes.models import Ingredient, Tag
 
-from recipes.models import Ingredient
+TABLES = {
+    Ingredient: 'ingredients.csv',
+    Tag: 'tags.csv'
+}
+
+CSV_FILE_PATH = BASE_DIR / 'data'
 
 
 class Command(BaseCommand):
-    help = 'Импортирует данные из CSV-файлов в базу данных'
 
-    def load_ingredients(self):
-        path = f'{settings.BASE_DIR}/data/ingredients.csv'
-        with open(path, encoding='utf-8') as csvfile:
-            reader = csv.reader(csvfile)
-            try:
-                for row in reader:
-                    recipes_igredient = Ingredient.objects.create(
-                        name=row[0],
-                        measurement_unit=row[1]
-                    )
-                    self.stdout.write(self.style.SUCCESS(
-                        f'Ingredient {recipes_igredient.name} создан.'
-                    ))
-            except Ingredient.DoesNotExist:
-                self.stderr.write(self.style.ERROR('Ingredient не создан'))
-            except ValueError as error:
-                self.stderr.write(self.style.ERROR(f'Error: {str(error)}'))
-
-    def handle(self, *args, **options):
-        self.load_ingredients()
-
-        self.stdout.write(self.style.SUCCESS('База данных импортирована '
-                                             'полностью.'))
+    def handle(self, *args, **kwargs):
+        for model, csv_f in TABLES.items():
+            with open(
+                    CSV_FILE_PATH / csv_f, encoding='utf-8'
+            ) as csv_file:
+                for record in csv.DictReader(csv_file):
+                    model.objects.get_or_create(**record)
+        self.stdout.write(
+            self.style.SUCCESS('Success write to database'))
