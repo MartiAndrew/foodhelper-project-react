eb4e26056fbf751c7027758ae509df4a0ea62f63
Andrew <andrew.mishkov@gmail.com>
2023-07-13 15:04:52 +0300

    fix urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,18 +1,18 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .v1.users.views import UserViewSet
-from .v1.tags.views import TagViewSet
+from .v1.users.views import CustomUserViewSet
 from .v1.ingredient.views import IngredientViewSet
 from .v1.recipe.views import RecipeViewSet
+from .v1.tags.views import TagViewSet
 
 app_name = 'api'
 
 router_v1 = DefaultRouter()
-router_v1.register(r'users', UserViewSet, basename='users')
-router_v1.register(r'tags', TagViewSet, basename='tags')
-router_v1.register(r'ingredients', IngredientViewSet, basename='ingredients')
-router_v1.register(r'recipes', RecipeViewSet, basename='recipes')
+router_v1.register('users', CustomUserViewSet, basename='users')
+router_v1.register('tags', TagViewSet, basename='tags')
+router_v1.register('ingredients', IngredientViewSet, basename='ingredients')
+router_v1.register('recipes', RecipeViewSet, basename='recipes')
 
 urlpatterns = [
     path('', include(router_v1.urls)),
