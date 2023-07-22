42a598fc0468ff59ca85b628cc84700f49b91fa6
Andrew <andrew.mishkov@gmail.com>
2023-07-22 17:27:30 +0300

    fix ALL 8 end

--- backend/foodhelper/settings.py
+++ backend/foodhelper/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django')
 
 DEBUG = os.getenv('DEBUG', 'false').lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", default='localhost').split(',')
+ALLOWED_HOSTS = ['127.0.0.1', '158.160.30.247', 'localhost', 'foodhelper.ddns.net']
 
 PROJECT_APPS = [
     'users.apps.UsersConfig',
