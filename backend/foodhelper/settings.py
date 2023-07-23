a8bf0db638a92f1834f8bdbe2b7e3d894056d06e
Andrew <andrew.mishkov@gmail.com>
2023-07-23 11:05:30 +0300

    fix 10 All

--- backend/foodhelper/settings.py
+++ backend/foodhelper/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-env_path = Path(__file__).resolve().parent.parent.parent / '.env'
+env_path = Path(__file__).resolve().parent.parent / '.env'
 load_dotenv(dotenv_path=env_path)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
